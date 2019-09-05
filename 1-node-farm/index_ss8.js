5290764b0c845612106938ca2682991f440ce05a
Tony Annobil <tonyannobil@Tonys-MacBook-Air.local>
2019-09-05 14:30:21 +0100

    190905: index_ss8,9 update4

---
+++ 1-node-farm/index_ss8.js
@@ -0,0 +1,17 @@
+//DOCUMENTATION 
+//https://nodejs.org/dist/latest-v10.x/docs/api/
+
+
+const fs = require('fs'); // 'require' the file system (fs) to read from and write to files 
+
+
+// BLOCKING SYNCHRONOUS WAY
+// read data from the file (input.txt), place file into a variable textIn
+const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8'); 
+// Log textIn to console
+console.log(textIn); 
+// Use ES6 syntax to insert text and Javascript into a file 
+const textOut = `This is what we know about the avocado: ${textIn}. Created on ${Date.now()}.`; 
+fs.writeFileSync('starter/output.txt', textOut);
+console.log('File has been written!')
+
